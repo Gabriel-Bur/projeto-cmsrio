da287ca640b342ce5af7139059cca2488f472bcb
Gabriel Bur <gabriel.bur@al.infnet.edu.br>
2018-05-16 16:15:13 -0300

    EF adicionado ao Web api

--- cmsrio/Modelo/Classes/Avaliacao.cs
+++ cmsrio/Modelo/Classes/Avaliacao.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Modelo.Classes
 {
-    class Avaliacao
+    public class Avaliacao
     {
+        public int IDAvaliacao { get; set; }
+
+
+        public int IDHospital { get; set; }
+
+
+        public string Nome { get; set; }
+
+
+        public string Email { get; set; }
+
+
+        public string Topico { get; set; }
+
+
+        public string Comentario { get; set; }
+
+
+        public string Data { get; set; }
     }
 }
---
+++ cmsrio/WebApi/Contexto/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Modelo.Classes;
+
+
+
+namespace WebApi.Contexto
+{
+    public class Contexto:DbContext
+    {
+        public Contexto() : base("DefaultConnection") { }
+
+        public DbSet<Hospital> Hospitais { get; set; }
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+    }
+
+
+
+}
